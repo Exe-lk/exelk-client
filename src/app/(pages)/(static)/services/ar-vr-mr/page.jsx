d4d32b12ef4e7cd69c3313c5767b34c976609afe
Geethanimal <geethanimal0@gmail.com>
2024-03-27 05:29:13 +0530

    Update3 27/3/2024
    SEO

---
+++ src/app/(pages)/(static)/services/ar-vr-mr/page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ServiceProfile from "@/components/service-profile/service-profile";
+import Footer from "@/components/footer/footer";
+
+const ArVrMr = () => {
+    return(
+        <div>
+            <ServiceProfile Title={'AR VR & MR'} Description={'\n' +
+                'Dive into new realities with Exe.lk. Our Augmented Reality (AR), Virtual Reality (VR), and Mixed Reality (MR) solutions open doors to immersive experiences, transforming how you connect with customers and innovate in your space. Step into a world where your imagination is the only limit. Welcome to the future, made real today.'} Title2={'Immersive Marketing'} Description2={'Elevate your brand with AR, VR, and MR, creating unforgettable customer engagements that drive interest and loyalty.'} Title3={'Training & Education'} Description3={'Transform learning and development with interactive experiences that enhance understanding and retention in ways traditional methods cannot match.'} Title4={'Virtual Showrooms'} Description4={'Step into our virtual spaces to showcase products and designs, offering a hands-on feel without physical boundaries.'} Title5={'Interactive Events'} Description5={'Bring your events to life with virtual participation options, expanding your audience and making every moment more engaging and inclusive.'} ImgSrc2={'/Images/services/ar-vr-mr/2.png'} ImgSrc3={'/Images/services/ar-vr-mr/3.jpg'} ImgSrc4={'/Images/services/ar-vr-mr/4.jpg'} ImgSrc5={'/Images/services/ar-vr-mr/5.jpeg'} />
+            <Footer/>
+        </div>
+    )
+}
+
+export default ArVrMr
--- src/components/section2/section2.jsx
+++ src/components/section2/section2.jsx
@@ -10,14 +10,14 @@ export default function Section2(){
                 <div class="container sec2_content">
                     <h2 className="mb-4 display-6">Our Services</h2>
                     <div className="row">
-                        <ServiceCard header="Research & Development" body="Fueling Innovation: Empowering Your R&D with Our Expert Software Services!" hreflink="pages/services/research&development"/>
-                        <ServiceCard header="AI & Machine Learning" body="Unleash the Power of AI & Machine Learning: Elevate Your Business with Our Expertise!" hreflink="pages/services/ai&machinelearning"/>
-                        <ServiceCard header="AR, VR and MR" body="Step into a New Reality: Experience the Future with AR, VR, and MR Solutions!" hreflink="pages/services/arvrmr"/>
-                        <ServiceCard header="Web, Mobile & Pc Application" body="Seamless Digital Experiences: Elevate Your Reach with Expert Web, Mobile, and PC Applications!" hreflink="pages/services/softwareapplications"/>
-                        <ServiceCard header="Robotics & IOT" body="Transforming Industries: Embrace the Future with Robotics & IoT Solutions!" hreflink="pages/services/robotics&iot"/>
-                        <ServiceCard header="Network & Security" body="Fortify Your Foundation: Safeguard Your Business with Network & Security Expertise!" hreflink="pages/services/networkandsecurity"/>
-                        <ServiceCard header="Blockchain Development" body="Unlocking Digital Trust: Explore Possibilities with Blockchain Development!" hreflink="pages/services/blockchain"/>
-                        <ServiceCard header="Product Conceptualization" body="From Idea to Reality: Shape Your Vision with Expert Product Conceptualization!" hreflink="pages/services/productconceptualization"/>
+                        <ServiceCard header="Research & Development" body="Fueling Innovation: Empowering Your R&D with Our Expert Software Services!" hreflink="services/research-and-development"/>
+                        <ServiceCard header="AI & Machine Learning" body="Unleash the Power of AI & Machine Learning: Elevate Your Business with Our Expertise!" hreflink="services/ai-and-machinelearning"/>
+                        <ServiceCard header="AR, VR and MR" body="Step into a New Reality: Experience the Future with AR, VR, and MR Solutions!" hreflink="services/ar-vr-mr"/>
+                        <ServiceCard header="Web, Mobile & Pc Application" body="Seamless Digital Experiences: Elevate Your Reach with Expert Web, Mobile, and PC Applications!" hreflink="services/software-applications"/>
+                        <ServiceCard header="Robotics & IOT" body="Transforming Industries: Embrace the Future with Robotics & IoT Solutions!" hreflink="services/robotics-and-iot"/>
+                        <ServiceCard header="Network & Security" body="Fortify Your Foundation: Safeguard Your Business with Network & Security Expertise!" hreflink="services/network-and-security"/>
+                        <ServiceCard header="Blockchain Development" body="Unlocking Digital Trust: Explore Possibilities with Blockchain Development!" hreflink="services/blockchain-development"/>
+                        <ServiceCard header="Product Conceptualization" body="From Idea to Reality: Shape Your Vision with Expert Product Conceptualization!" hreflink="services/product-conceptualization"/>
                     </div>
                 </div>
             </div>
