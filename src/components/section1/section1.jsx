51544abadb08144a68b591e1dacd153dff268491
Geethanimal <geethanimal0@gmail.com>
2024-04-14 17:18:20 +0530

    Update9 14/4/2024

--- src/components/section1/section1.jsx
+++ src/components/section1/section1.jsx
@@ -4,17 +4,16 @@ import "./section1.scss"
 export default function Section1(){
     return(
 
-        <div id="section1" className="section1 container-fluid bg-black">
-
-            <div className="sec1_content mt-5 pt-5 pb-5">
-                <h2 className="mt-3 display-7 text-center">Who We Are</h2>
-                <p className="mt-3 mx-5 fs-6 text lead text-light text-center">Welcome to Exe.lk (Pvt) Ltd, where we turn ideas into reality! With innovation at our core, our skilled team crafts cutting-edge solutions that empower businesses and individuals alike. From web applications to mobile solutions, our tailored approach ensures we not only meet but exceed expectations. Whether you are an aspiring entrepreneur or a visionary enterprise, we are here to transform your concepts into tangible success. Embrace this journey of innovation with us, and together, let&aposs create something extraordinary. Welcome to Exe.lk (Pvt) Ltd, where your ideas take flight – making the impossible possible!</p>
+        <div id="section1" className="section1 container-fluid bg-black ">
+            <div className="sec1_content pt-5 pb-5">
+                <h2 className="pt-3 display-7 text-center">Who We Are</h2>
+                <p className="pt-3 mx-5 fs-6 text lead text-light text-center">Welcome to Exe.lk (Pvt) Ltd, where we turn ideas into reality! With innovation at our core, our skilled team crafts cutting-edge solutions that empower businesses and individuals alike. From web applications to mobile solutions, our tailored approach ensures we not only meet but exceed expectations. Whether you are an aspiring entrepreneur or a visionary enterprise, we are here to transform your concepts into tangible success. Embrace this journey of innovation with us, and together, let&aposs create something extraordinary. Welcome to Exe.lk (Pvt) Ltd, where your ideas take flight – making the impossible possible!</p>
                 <div className="sec1_content_footer">
-                    <h5 className="mt-5 text-center"><a className={'text-decoration-none'} href="/about-us">Read More</a></h5>
+                    <h5 className="pt-5 text-center"><a className={'text-decoration-none'} href="/about-us">Read More</a></h5>
                 </div>
             </div>
             <div className={'technologies pt-5 pb-5'}>
-                <h2 className={'display-7 text-center mb-5'}>We are experts in</h2>
+                <h2 className={'display-7 text-center pb-5'}>We are experts in</h2>
                 <div className={'technology_brand_logos'}>
                     <div className={'technology_brand_logos_slide'}>
                         <img src="/Images/sec1/expertise/gcp.png" className="img-fluid" alt="..." />
