3b8650e1ea858db252ab18c52ee0edddf673ed56
Geethanimal <geethanimal0@gmail.com>
2024-04-14 17:21:35 +0530

    Update10 14/4/2024

--- src/components/section5/section5.jsx
+++ src/components/section5/section5.jsx
@@ -6,7 +6,7 @@ import "./section5.scss"
 export default function Section5(){
     return(
         <div className={'section5 bg-black'}>
-            <div id="section5" className="section5 conainer-fluid pt-5">
+            <div id="section5" className="section5 conainer-fluid">
                 <div className="container pt-5 pb-5">
                     <h2 className="section5Head text-center display-6 pt-5 pb-5">Contact Us</h2>
                     <ContactUs/>
